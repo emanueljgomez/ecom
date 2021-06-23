75a9c5774657251d232f3003e270d1659391a5f2
Emanuel Gomez <emanueljgomez@gmail.com>
2021-06-23 20:43:55 -0300

    Login component: basic layout and styles

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder,FormGroup,Validators } from '@angular/forms';  // Importar herramientos de ReactiveFormsModule
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  myForm:FormGroup  // Declarar variable para el formulario con tipo FormGroup
+
+  constructor(
+    private fb:FormBuilder  // Inyección de Dependecias -- Declarar variable para FormBuilder
+  ) { 
+    // Construcción de formulario:
+    this.myForm=this.fb.group({
+      user: ["",[Validators.required]],
+      password: ["",[Validators.required]]
+    })
+  }
 
   ngOnInit(): void {
   }
--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms'; // Importar Modulos para formularios
 
-
+import { MatCardModule } from '@angular/material/card'; // Importar Componente de Angular Material
+import { MatButtonModule } from '@angular/material/button'; // Importar Componente de Angular Material
+import { MatFormFieldModule } from '@angular/material/form-field'; // Importar Componente de Angular Material
 
 @NgModule({
   declarations: [
     LoginComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule, ReactiveFormsModule, // Declarar importación de Modulos para formularios
+    MatCardModule, // Declarar importación de Componente de Angular Material
+    MatButtonModule, // Declarar importación de Componente de Angular Material
+    MatFormFieldModule // Declarar importación de Componente de Angular Material
   ],
   exports:[LoginComponent]   // Línea creada manualmente - Permite que cualquier módulo consuma los componentes de este módulo
 })
