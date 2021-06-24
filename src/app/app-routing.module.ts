2cef6679f1396f93cd3e7e6e08b984bccc8f3fb5
Emanuel Gomez <emanueljgomez@gmail.com>
2021-06-24 16:33:07 -0300

    Added base routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './modules/home/home.component';
+import { LoginComponent } from './modules/login/login.component';
+import { RegistroComponent } from './modules/registro/registro.component';
 
-const routes: Routes = [];
+const routes: Routes = [  // Definir rutas de la aplicación
+  {path:"",component:HomeComponent},
+  {path:"login",component:LoginComponent},
+  {path:"signup",component:RegistroComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
