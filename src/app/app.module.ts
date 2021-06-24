af40d8e06954c3b80cf10462bc533f5fcc27b4fb
Emanuel Gomez <emanueljgomez@gmail.com>
2021-06-24 17:18:18 -0300

    Added basic navigation and proper routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatToolbarModule} from '@angular/material/toolbar';     // Material para NavBar
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DetalleComponent } from './modules/detalle/detalle.component';
-import { DetalleModule } from './modules/detalle/detalle.module';       // Autoimport - Línea 18
-import { HomeModule } from './modules/home/home.module';                // Autoimport - Línea 19
-import { LoginModule } from './modules/login/login.module';             // Autoimport - Línea 20
-import { RegistroModule } from './modules/registro/registro.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';    // Autoimport - Línea 21
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DetalleModule } from './modules/detalle/detalle.module';       // Autoimport - Línea 26
+import { HomeModule } from './modules/home/home.module';                // Autoimport - Línea 27
+import { LoginModule } from './modules/login/login.module';             // Autoimport - Línea 28
+import { RegistroModule } from './modules/registro/registro.module';    // Autoimport - Línea 29
+
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
     AppRoutingModule,
     DetalleModule,    // Modulo custom importado
     HomeModule,       // Modulo custom importado
     LoginModule,      // Modulo custom importado
-    RegistroModule, BrowserAnimationsModule    // Modulo custom importado
+    RegistroModule,   // Modulo custom importado
   ],
   providers: [],
   bootstrap: [AppComponent],
